86f36f99331c46a273141b4ef493e7c136a88d05
Hushino <frostshock@outlook.com>
2018-04-22 01:21:29 -0300

    dsfsdf
    Signed-off-by: Hushino <frostshock@outlook.com>

--- RedOver/src/controller/cEpisodes/CapsHome.java
+++ RedOver/src/controller/cEpisodes/CapsHome.java
@@ -20,7 +20,6 @@ public class CapsHome extends HttpServlet {
 		super();
 
 	}
-
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
@@ -34,7 +33,6 @@ public class CapsHome extends HttpServlet {
 
 		processRequest(request, response);
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
