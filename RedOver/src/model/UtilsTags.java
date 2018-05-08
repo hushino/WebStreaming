881a1e068c78d09ec29424651bf90c7548cd350c
Hushino <frostshock@outlook.com>
2018-05-08 19:52:16 -0300

    fex
    Signed-off-by: Hushino <frostshock@outlook.com>

--- RedOver/src/model/UtilsTags.java
+++ RedOver/src/model/UtilsTags.java
@@ -1,10 +1,8 @@
 package model;
 
- 
 import java.util.ArrayList;
 import java.util.List;
 
- 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -14,23 +12,20 @@ import hibernateUtil.HibernateUtil;
 public class UtilsTags {
 	Session session = null;
 	Transaction transaction = null;
-	
+
 	public List<Tags> getTags() {
 		session = HibernateUtil.getSessionFactory().openSession();
-		/*@SuppressWarnings("unchecked")
-		List<Tags> tags = session.createQuery("from Tags").list();*/
-		
 		ArrayList<Tags> arreglo = new ArrayList<>();
-		for (Object object : session.createQuery("from Tags").getResultList()
-				 
-			)
-		{
+		for (Object object : session.createQuery("from Tags")
+				//.setHint("org.hibernate.cacheable", true)
+				.getResultList()
+
+		) {
 			arreglo.add((Tags) object);
 		}
 		session.close();
 		return arreglo;
 	}
-	
 	public Tags findbyId(Long id) {
 		session = HibernateUtil.getSessionFactory().openSession();
 		transaction = session.getTransaction();
@@ -40,5 +35,5 @@ public class UtilsTags {
 		session.close();
 		return tags;
 	}
-	
+
 }
