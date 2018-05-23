8088787faea20d6a70b5d266d6b8772526a6372d
Hushino <frostshock@outlook.com>
2018-05-23 00:17:12 -0300

    lik
    Signed-off-by: Hushino <frostshock@outlook.com>

--- RedOver/src/controller/Add.java
+++ RedOver/src/controller/Add.java
@@ -22,7 +22,7 @@ public class Add extends HttpServlet {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-
+	 
 		UtilsAnime op = new UtilsAnime();
 		
 		/*String[] tec = request.getParameterValues("tags");
